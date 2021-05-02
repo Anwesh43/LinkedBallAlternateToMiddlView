7b10293ba9dd2de963178caac68504e0772c773e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-03 00:27:25 +0530

    adding constants and libraries

--- ballalternatetomiddleview/src/main/java/com/example/ballalternatetomiddleview/BallAlternateToMiddleView.kt
+++ ballalternatetomiddleview/src/main/java/com/example/ballalternatetomiddleview/BallAlternateToMiddleView.kt
@@ -1,2 +1,26 @@
 package com.example.ballalternatetomiddleview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "",
+    "",
+    "",
+    "",
+    ""
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val circles : Int = 5
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 4.5f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
