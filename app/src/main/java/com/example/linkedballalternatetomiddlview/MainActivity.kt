168bb6a3e6538f0419f9fd1175f029f8aec40c80
Anwesh43 <anweshthecool0@gmail.com>
2021-05-03 01:16:12 +0530

    adding view in actvity

--- app/src/main/java/com/example/linkedballalternatetomiddlview/MainActivity.kt
+++ app/src/main/java/com/example/linkedballalternatetomiddlview/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.linkedballalternatetomiddlview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.ballalternatetomiddleview.BallAlternateToMiddleView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BallAlternateToMiddleView.create(this)
         fullScreen()
     }
 }
