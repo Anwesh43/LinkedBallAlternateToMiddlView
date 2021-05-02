c4fde8bc247a18c0aff701996f7f9af8bcb1d34c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-03 00:59:15 +0530

    adding BATMNode class

--- ballalternatetomiddleview/src/main/java/com/example/ballalternatetomiddleview/BallAlternateToMiddleView.kt
+++ ballalternatetomiddleview/src/main/java/com/example/ballalternatetomiddleview/BallAlternateToMiddleView.kt
@@ -121,4 +121,45 @@ class BallAlternateToMiddleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BATMNode(var i : Int, val state : State = State()) {
+
+        private var next : BATMNode? = null
+        private var prev : BATMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size -1) {
+                next = BATMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBATMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BATMNode {
+            var curr : BATMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
