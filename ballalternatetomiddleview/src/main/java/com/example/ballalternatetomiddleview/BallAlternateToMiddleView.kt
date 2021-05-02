9339cc1fc9b4090749fcda5aeff4968b7f368e96
Anwesh43 <anweshthecool0@gmail.com>
2021-05-03 00:53:02 +0530

    adding BallAlternateToMiddleView class

--- ballalternatetomiddleview/src/main/java/com/example/ballalternatetomiddleview/BallAlternateToMiddleView.kt
+++ ballalternatetomiddleview/src/main/java/com/example/ballalternatetomiddleview/BallAlternateToMiddleView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawBATMNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBallAlternateToMiddle(scale, w, h, paint)
 }
+
+class BallAlternateToMiddleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
